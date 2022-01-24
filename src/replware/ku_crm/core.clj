ea801a49099592a8cd35ba531a84f30cc0190ce2
Laurence Chen <humorless@gmail.com>
2022-01-25 02:13:10 +0800

    add two basic commands into main

--- src/replware/ku_crm/core.clj
+++ src/replware/ku_crm/core.clj
@@ -1,10 +1,20 @@
 (ns replware.ku-crm.core
   (:require [replware.ku-crm.db :as db]
-            [replware.ku-crm.txor :as txor])
+            [replware.ku-crm.txor :as txor]
+            [replware.ku-crm.dsync :as dsync])
   (:gen-class))
 
 (defn -main
-  "java -jar ku-crm-1.0.0-standalone.jar $DB_FILE_PATH"
+  "java -jar ku-crm-1.0.0-standalone.jar $CMD $DB_FILE_PATH"
   [& args]
-  (let [dtlv-path (first args)]
-    (txor/import-from-db! dtlv-path)))
+  (let [cmd (first args)
+        dtlv-path (second args)]
+    (cond
+      (= cmd "import") (txor/import-from-db! dtlv-path)
+      (= cmd "sync") (dsync/->ops-students-table))))
+
+;;TODO
+;; - provide some options:
+;;   1. import-from-db
+;;   2. sync-to-db
+;;   3. CRUD
