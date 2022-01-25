a6730ce4b3b75a378a78e61698b663ac6e236b13
Laurence Chen <humorless@gmail.com>
2022-01-25 16:54:47 +0800

    add specifying db schema ability to bin/ops_students.sh

--- bin/ops_students.sh
+++ bin/ops_students.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+PGOPTIONS="--search_path=dbt_develop"
+export PGOPTIONS
 
 psql -d kuops_dev -c "DROP TABLE IF EXISTS ops_students;"
 psql -d kuops_dev -c "CREATE TABLE ops_students (id text PRIMARY KEY, name text, birth DATE, telephone text, mobile text, classroom_id text, classroom_type text, old_id text);"
