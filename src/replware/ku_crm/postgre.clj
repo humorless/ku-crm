44b960b8d76351405d2508712de02a2632c2586e
Laurence Chen <humorless@gmail.com>
2022-01-29 02:50:31 +0800

    change from hugsql to honeysql

--- src/replware/ku_crm/postgre.clj
+++ src/replware/ku_crm/postgre.clj
@@ -4,7 +4,8 @@
   PostgreSQL does not seem to perform a conversion from `java.util.Date` to a SQL
   data type automatically. I need to require the next.jdbc.date-time namespace to
   enable that conversion."
-  (:require [conman.core :as conman]
+  (:require [honey.sql :as sql]
+            [next.jdbc :as jdbc]
             [next.jdbc.date-time]
             [clojure.java.io :as io]
             [aero.core :as aero]))
@@ -12,12 +13,15 @@
 (def config (aero/read-config
               (io/resource "config.edn")))
 
-(def db-entry {:jdbc-url
+(def db-entry {:jdbcUrl
                (:database-url config)})
 
-(def ^:dynamic *db* (conman/connect! db-entry))
+(def conn (jdbc/get-datasource db-entry))
 
-(conman/bind-connection *db* "sql/queries.sql")
+(def sqlmap {:select [:*]
+             :from [:students]})
+
+(jdbc/execute! conn (sql/format sqlmap))
 
 (comment
   (get-all-students)
